9f87f4e7cbeaac5932b7580207a200b4ac04e0e3
elijahrosett <rosett3@gmail.com>
2022-05-05 13:49:39 -0700

    Created Post endpoint and function with 201 response

--- music/views.py
+++ music/views.py
@@ -1,14 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Music
 from .serializers import MusicSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def music_list(request):
     music = Music.objects.all()
-    serializer = MusicSerializer(music, many=True)
-
-
-
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = MusicSerializer(music, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = MusicSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
