7bdeb0212c3456964b3fd00331ed050d6ef3ba7e
elijahrosett <rosett3@gmail.com>
2022-05-05 08:55:39 -0700

    Created models for songs app

---
+++ songs/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    realese_date = models.DateField()
+    genre = models.CharField(max_length=255)
