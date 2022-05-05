a1fbfced0d68d98f16a624c1e6a1c09bc98eecd4
elijahrosett <rosett3@gmail.com>
2022-05-05 13:16:45 -0700

    Created a get function to retreive all music

--- music/models.py
+++ music/models.py
@@ -5,5 +5,5 @@ class Music(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
     album = models.CharField(max_length=255)
-    realese_date = models.DateField()
+    release_date = models.DateField()
     genre = models.CharField(max_length=255)
--- music/serializers.py
+++ music/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Music
 
+
 class MusicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Music
-        fields = {"id", "artist", "album", "release_Date", "genre"}
+        fields = ['id', "title", "artist", "album", "release_date", "genre"]
