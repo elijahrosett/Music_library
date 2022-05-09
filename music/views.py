193cfa01c6d97c2fb02f3e10262b961ca09e44bd
elijahrosett <rosett3@gmail.com>
2022-05-09 08:27:38 -0700

    Corrected delete function to return deleted item

--- music/views.py
+++ music/views.py
@@ -32,5 +32,6 @@ def music_detail(request, pk):
         serializer.save()
         return Response(serializer.data)
     elif request.method == 'DELETE':
-        music.delete
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        serializers = MusicSerializer(music)
+        music.delete()
+        return Response(serializers.data)
